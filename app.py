c78230dac36928a4d7dc28f089cbecaa499db7c1
cgoyal208 <chiraggoyal3108@gmail.com>
2026-09-05 04:16:27 +0000

    Prepare app for hosted deployment

--- app.py
+++ app.py
@@ -171,4 +171,8 @@ app = create_app()
 
 if __name__ == "__main__":
     # Allow access from all network interfaces, not just localhost
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", "5000")),
+        debug=os.environ.get("FLASK_DEBUG", "false").lower() == "true",
+    )
